205cb9bb789a93b88ec8716f4177325892fd1f75
oneqimt <oneqimt@gmail.com>
2023-05-15 09:32:21 -0700

    Initial commit

---
+++ imtmobileapps/data_util.py
@@ -0,0 +1,23 @@
+import os
+import openai
+import settings as s
+
+openai.api_key = s.OPENAI_API_KEY
+
+
+def complete(prompt):
+    # query text-davinci-003
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        temperature=0,
+        max_tokens=400,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    return response['choices'][0]['text'].strip()
+
+
+
